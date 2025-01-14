25901aa628441b0586495ab0e8592745b5bb0bbb
inksne <debanyj23@gmail.com>
2025-01-14 18:48:20 +0300

    remove declarative_base

--- database/database.py
+++ database/database.py
@@ -1,11 +1,10 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
+
+from .models import Base
 
 DATABASE_URL = "sqlite+aiosqlite:///./database.db"
-
-Base = declarative_base()
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
